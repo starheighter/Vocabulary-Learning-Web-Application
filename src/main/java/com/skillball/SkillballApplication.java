743e1aa83241e6ed3f7cf6ee7f15f5b76da8775c
dominik <dominik.larche@googlemail.com>
2025-07-26 12:23:05 +0200

    First Commit

--- src/main/java/com/skillball/SkillballApplication.java
+++ src/main/java/com/skillball/SkillballApplication.java
@@ -3,6 +3,7 @@ package com.skillball;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://localhost:8080
 @SpringBootApplication
 public class SkillballApplication {
     public static void main(String[] args) {
